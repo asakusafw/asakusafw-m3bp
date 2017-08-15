2069a40c9c5f02210eacc094f09ebae21f07df56
Suguru ARAKAWA <arakawa@nautilus-technologies.com>
2017-08-15 16:24:32 +0900

    Add test of `asakusaUpgrade` with Gradle 4.1.

--- gradle/src/test/groovy/com/asakusafw/m3bp/gradle/plugins/AsakusaUpgradeTest.groovy
+++ gradle/src/test/groovy/com/asakusafw/m3bp/gradle/plugins/AsakusaUpgradeTest.groovy
@@ -51,6 +51,14 @@ class AsakusaUpgradeTest {
     }
 
     /**
+     * Test for {@code 4.1} (Asakusa on M3BP {@code 0.3.0}).
+     */
+    @Test
+    void 'v4.1'() {
+        doUpgradeFromTestName()
+    }
+
+    /**
      * Test for {@code 3.4.1} (Asakusa on M3BP {@code 0.2.1}).
      */
     @Test
